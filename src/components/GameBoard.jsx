5a76a86fd4960551ecff7a1237a4f3be158448a2
Riya Bhutada <riyabhutada27@gmail.com>
2025-02-16 14:33:47 -0600

    Adding Tic-Tac-Toe React JS project

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,35 @@
+// import {useState} from "react";
+
+
+
+export default function GameBoard({onSelectedSquare, board}){
+
+    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    // function handleSelectedSquare(rowIndex, colIndex){
+    //     setGameBoard((prevGameBoard) => {
+    //         const updatedBoard = [...prevGameBoard.map((innerArray) => [...innerArray])];
+    //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+    //         return updatedBoard;
+    //     });
+    //     onSelectedSquare();
+    // }
+
+    return (
+        <ol id="game-board">
+            {board.map((row, rowIndex) => <li key={rowIndex}>
+                <ol>
+                    {row.map((playerSymbol, colIndex) => (
+                        <li key="colIndex">
+                            <button
+                                onClick={() => onSelectedSquare(rowIndex, colIndex)}
+                                disabled={playerSymbol != null}
+                            >
+                                {playerSymbol}
+                            </button>
+                        </li>))}
+                </ol>
+            </li>)}
+        </ol>
+    );
+}
